e2aa46c72e3b8a76e14b828d60e57b084a2cbfe5
sharad.goyal <sharad.goyal@alignedautomation.com>
2023-09-05 01:24:07 +0530

    SG: fixes

--- terra.tf
+++ terra.tf
@@ -26,61 +26,61 @@ resource "aws_vpc" "example" {
   }
 }
 
-# Create a subnet in the VPC
-resource "aws_subnet" "example" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"  # Change to your desired availability zone
-  tags = {
-    Name = "${var.appName}-${var.env}-Subnet"
-    Company = var.tagValue1
-    Environment = var.env
-  }
-}
+# # Create a subnet in the VPC
+# resource "aws_subnet" "example" {
+#   vpc_id     = aws_vpc.example.id
+#   cidr_block = "10.0.0.0/24"
+#   availability_zone = "us-east-1a"  # Change to your desired availability zone
+#   tags = {
+#     Name = "${var.appName}-${var.env}-Subnet"
+#     Company = var.tagValue1
+#     Environment = var.env
+#   }
+# }
 
-# Create a security group
-resource "aws_security_group" "example" {
-  name        = "${var.appName}-${var.env}-SecurityGroup"
-  description = "Allow SSH and HTTP traffic"
-  vpc_id      = aws_vpc.example.id
+# # Create a security group
+# resource "aws_security_group" "example" {
+#   name        = "${var.appName}-${var.env}-SecurityGroup"
+#   description = "Allow SSH and HTTP traffic"
+#   vpc_id      = aws_vpc.example.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (for demonstration purposes)
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (for demonstration purposes)
+#   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere (for demonstration purposes)
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere (for demonstration purposes)
+#   }
 
-  tags = {
-    Name = "${var.appName}-${var.env}-SecurityGroup"
-    Company = var.tagValue1
-    Environment = var.env
-  }
-}
+#   tags = {
+#     Name = "${var.appName}-${var.env}-SecurityGroup"
+#     Company = var.tagValue1
+#     Environment = var.env
+#   }
+# }
 
-# Launch an EC2 instance in the subnet
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Use your desired AMI ID
-  instance_type = "t2.micro"  # Use your desired instance type
-  subnet_id     = aws_subnet.example.id
-  security_groups = [aws_security_group.example.name]
+# # Launch an EC2 instance in the subnet
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c55b159cbfafe1f0"  # Use your desired AMI ID
+#   instance_type = "t2.micro"  # Use your desired instance type
+#   subnet_id     = aws_subnet.example.id
+#   security_groups = [aws_security_group.example.name]
 
-  tags = {
-    Name = "${var.appName}-${var.env}-EC2Instance"
-    Company = var.tagValue1
-    Environment = var.env
-  }
-}
+#   tags = {
+#     Name = "${var.appName}-${var.env}-EC2Instance"
+#     Company = var.tagValue1
+#     Environment = var.env
+#   }
+# }
 
-# Output the public IP address of the EC2 instance
-output "ec2_instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
+# # Output the public IP address of the EC2 instance
+# output "ec2_instance_public_ip" {
+#   value = aws_instance.example.public_ip
+# }
 
